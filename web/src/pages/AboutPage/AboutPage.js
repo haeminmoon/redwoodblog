22bbc27bec39e81c5953f33672b0873f183f98f2
Harace <mym0846@gmail.com>
2020-12-24 01:37:18 +0900

    first commit

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,9 @@
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>This is the AboutPage</BlogLayout>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const HomePage = () => {
+  return (
+    <BlogLayout>This is the HomePage</BlogLayout>
+  )
+}
+
+export default HomePage
